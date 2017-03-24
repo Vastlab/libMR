13e54756a6d7bc447c4937292a672b9b36915930
Manuel Gunther <siebenkopf@googlemail.com>
2017-03-24 15:54:47 -0600

    Switched to setuptools in setup.py

---
+++ README.rst
@@ -0,0 +1,14 @@
+LIBMR
+=====
+
+
+libMR provides core MetaRecognition and  Weibull fitting functionality.
+It is used to calculate w-scores used for multi-modal fusion.
+This version uses object, that retain the "transforms" (including save/load) need to compute the actual w-scores.
+It includes functions  for Prediction, CDF,  Inverse, FitHigh (larger is better), FitLow (smaller is better) and FitSVM.
+
+INSTALLATION
+++++++++++++
+
+pip install libmr
+
--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
-from distutils.core import setup
-from distutils.extension import Extension
-import sys
+from setuptools import setup
+from setuptools.extension import Extension
+import numpy
 
 try:
     from Cython.Build import cythonize
-    import numpy
 except ImportError:
-    print "You must have Cython >=0.17 and NumPy to build LibMR's python bindings!"
-    sys.exit(1)
-
-with open("README.rst") as f:
-    l_descrip=f.read()
+    raise ImportError("You must have Cython >=0.17 to build LibMR's python bindings!")
 
 setup(name='libmr',
-      ext_modules = cythonize(Extension('libmr',[
+      ext_modules = __import__("Cython").Build.cythonize(Extension('libmr',[
                             "python/libmr.pyx",
                             "libMR/MetaRecognition.cpp",
                             "libMR/weibull.c"
@@ -24,8 +19,8 @@ setup(name='libmr',
       data_files = [('libMR', ['libMR/MetaRecognition.h', 'libMR/weibull.h'])],
       version = "0.1.3",
       description="Python bindings for LibMR, the metarecognition library",
-      long_description=l_descrip,
-      url="http://www.metarecognition.com/ml-mr-2/",
+      long_description=open("README.rst").read(),
+      url="https://github.com/Vastlab/libMR",
       license="http://www.metarecognition.com/libmr-license/",
       author='Terry Boult, Ethan Rudd, and Manuel Gunther',
       install_requires=['cython','numpy']
